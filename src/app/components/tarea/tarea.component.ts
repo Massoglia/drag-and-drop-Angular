e6f2e7ead4695bea5d439ba1d85dff97a5337cfa
Massoglia <massogliad@outlook.com>
2023-06-22 21:17:59 +0100

    ok

--- src/app/components/tarea/tarea.component.ts
+++ src/app/components/tarea/tarea.component.ts
@@ -8,25 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareaComponent implements OnInit {
 
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the ' + 'Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker'
-  ];
+  todo: string[] = ['Programacion', 'Ingles', 'Testing Manual'];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-  }
+
 
 }
