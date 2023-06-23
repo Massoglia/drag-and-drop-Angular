55fd61a1fb919882ad8d0d189477e05abccb6e27
Massoglia <massogliad@outlook.com>
2023-06-23 16:47:33 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
+// Componentes //
 import { AppComponent } from './app.component';
 import { TareaComponent } from './components/tarea/tarea.component';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
